5d9bf333d769f8f4ff88bd20a1ef7e55bc09f6b2
Luciano <luciano.lopez.2019@berkeley.edu>
2018-04-26 13:11:57 -0700

    stuff

--- app/src/main/java/teamc/finalproject/MainActivity.java
+++ app/src/main/java/teamc/finalproject/MainActivity.java
@@ -3,11 +3,18 @@ package teamc.finalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WordListGenerator wlg = new WordListGenerator();
+        System.out.println("starting");
+        HashMap<String, String> words = wlg.getWordList(10);
+        System.out.println(words);
+        System.out.println("finished");
     }
 }
