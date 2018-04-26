9b1283761c3a9ab9125116b5c9c6e2b53b531682
Sagang Wee <sagangwee@Sagangs-MacBook-Pro.local>
2018-04-26 01:42:59 -0700

    found words

--- app/src/main/java/teamc/finalproject/RecyclerViewWords/WordsAdapter.java
+++ app/src/main/java/teamc/finalproject/RecyclerViewWords/WordsAdapter.java
@@ -13,31 +13,60 @@ import teamc.finalproject.VerificationActivity;
 import teamc.finalproject.R;
 import teamc.finalproject.Word;
 
-public class WordsAdapter extends RecyclerView.Adapter<WordViewHolder> {
+public class WordsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<Word> wordsList;
+    private int viewType;
     private Context context;
 
-    public WordsAdapter(List<Word> wordsList, Context context){
+    public WordsAdapter(List<Word> wordsList, int viewType, Context context){
         this.wordsList = wordsList;
+        this.viewType = viewType;
         this.context = context;
     }
+
     @Override
-    public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_word_item, null);
-        WordViewHolder rcv = new WordViewHolder(layoutView);
+    public int getItemViewType(int position) {
+        return viewType;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View layoutView;
+        RecyclerView.ViewHolder rcv = null;
+        switch (viewType) {
+            case 0:
+                layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_word_item, null);
+                rcv = new WordViewHolder(layoutView);
+                break;
+
+            case 1:
+                layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_found_word_item, null);
+                rcv = new FoundWordViewHolder(layoutView);
+                break;
+        }
         return rcv;
     }
 
     @Override
-    public void onBindViewHolder(final WordViewHolder holder, int position) {
-        holder.mWordTextView.setText(wordsList.get(position).getForeignTranslation());
-        holder.mCameraButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, VerificationActivity.class);
-                context.startActivity(intent);
-            }
-        });
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+        switch (holder.getItemViewType()) {
+            case 0:
+                WordViewHolder wordViewHolder = (WordViewHolder) holder;
+                wordViewHolder.mWordTextView.setText(wordsList.get(position).getForeignTranslation());
+                wordViewHolder.mCameraButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(context, VerificationActivity.class);
+                        context.startActivity(intent);
+                    }
+                });
+                break;
+
+            case 1:
+                FoundWordViewHolder foundWordViewHolder = (FoundWordViewHolder) holder;
+                foundWordViewHolder.mWordTextView.setText(wordsList.get(position).getForeignTranslation());
+                break;
+        }
     }
 
     @Override
--- app/src/main/java/teamc/finalproject/fragment/WordsFragment.java
+++ app/src/main/java/teamc/finalproject/fragment/WordsFragment.java
@@ -9,10 +9,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import teamc.finalproject.R;
 import teamc.finalproject.RecyclerViewWords.WordsAdapter;
+import teamc.finalproject.VerticalSpaceItemDecoration;
 import teamc.finalproject.Word;
 
 /**
@@ -24,11 +32,22 @@ import teamc.finalproject.Word;
  * create an instance of this fragment.
  */
 public class WordsFragment extends Fragment {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView mWordsRecyclerView;
+    private RecyclerView.Adapter mWordsAdapter;
+    private RecyclerView.LayoutManager mWordsLayoutManager;
+
+    private RecyclerView mFoundWordsRecyclerView;
+    private RecyclerView.Adapter mFoundWordsAdapter;
+    private RecyclerView.LayoutManager mFoundWordsLayoutManager;
 
     private OnFragmentInteractionListener mListener;
+
+    private static DatabaseReference mGamesRef;
+
+    private static String mGameId;
+    private static String mUserId;
+    private static ArrayList<Word> mWords;
+    private static ArrayList<Word> mFoundWords;
 
     public WordsFragment() {
         // Required empty public constructor
@@ -43,6 +62,14 @@ public class WordsFragment extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static WordsFragment newInstance() {
         WordsFragment fragment = new WordsFragment();
+        mGameId = "5961";
+        mUserId = "123456";
+
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference();
+        mGamesRef = db.child("games").child(mGameId);
+
+        mWords = new ArrayList<>();
+        mFoundWords = new ArrayList<>();
         return fragment;
     }
 
@@ -56,25 +83,79 @@ public class WordsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_words, container, false);
 
-        mRecyclerView = view.findViewById(R.id.recyclerView);
-        mRecyclerView.setNestedScrollingEnabled(false);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(getContext());
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new WordsAdapter(getDataSet(), getContext());
-        mRecyclerView.setAdapter(mAdapter);
+        VerticalSpaceItemDecoration dividerItemDecoration = new VerticalSpaceItemDecoration(48);
+
+        mWordsRecyclerView = view.findViewById(R.id.inProgressRecyclerView);
+        mWordsRecyclerView.addItemDecoration(dividerItemDecoration);
+        mWordsRecyclerView.setNestedScrollingEnabled(true);
+        mWordsRecyclerView.setHasFixedSize(true);
+        mWordsLayoutManager = new LinearLayoutManager(getContext());
+        mWordsRecyclerView.setLayoutManager(mWordsLayoutManager);
+        mWordsAdapter = new WordsAdapter(getWords(), 0, getContext());
+        mWordsRecyclerView.setAdapter(mWordsAdapter);
+
+        mFoundWordsRecyclerView = view.findViewById(R.id.foundRecyclerView);
+        mFoundWordsRecyclerView.addItemDecoration(dividerItemDecoration);
+        mFoundWordsRecyclerView.setNestedScrollingEnabled(true);
+        mFoundWordsRecyclerView.setHasFixedSize(true);
+        mFoundWordsLayoutManager = new LinearLayoutManager(getContext());
+        mFoundWordsRecyclerView.setLayoutManager(mFoundWordsLayoutManager);
+        mFoundWordsAdapter = new WordsAdapter(getFoundWords(), 1, getContext());
+        mFoundWordsRecyclerView.setAdapter(mFoundWordsAdapter);
 
         return view;
     }
 
-    private ArrayList<Word> results = new ArrayList<>();
-    private ArrayList<Word> getDataSet() {
-        listenForData();
-        return results;
+    private ArrayList<Word> getWords() {
+        DatabaseReference wordsRef = mGamesRef.child("words");
+        wordsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot child : snapshot.getChildren()) {
+                    mWords.add(child.getValue(Word.class));
+                    mWordsAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+        return mWords;
     }
 
-    private void listenForData() {
-        results.add(new Word("tree", "arbol"));
+    private ArrayList<Word> getFoundWords() {
+        DatabaseReference wordsFoundRef = mGamesRef.child("player_list").child(mUserId).child("words_found");
+        wordsFoundRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for (DataSnapshot child : dataSnapshot.getChildren()){
+                        Word foundWord = child.getValue(Word.class);
+                        if (!mFoundWords.contains(foundWord)) {
+                            mFoundWords.add(foundWord);
+                            mFoundWordsAdapter.notifyDataSetChanged();
+                        }
+                        updateWords(foundWord);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+        return mFoundWords;
+    }
+
+    private void updateWords(Word foundWord) {
+        for (Iterator<Word> iterator = mWords.iterator(); iterator.hasNext(); ) {
+            Word word = iterator.next();
+            if (word.getEnglishTranslation().equals(foundWord.getEnglishTranslation())) {
+                iterator.remove();
+            }
+        }
+        mWordsAdapter.notifyDataSetChanged();
     }
 
     // TODO: Rename method, update argument and hook method into UI event
