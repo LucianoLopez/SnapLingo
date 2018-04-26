9f4c05472104a18149dd6d1a8f8c24263ad14efd
adelapo <adelapo@berkeley.edu>
2018-04-26 12:40:02 -0700

    adding my changes

--- app/src/main/java/teamc/finalproject/FirebaseUtils.java
+++ app/src/main/java/teamc/finalproject/FirebaseUtils.java
@@ -2,56 +2,103 @@ package teamc.finalproject;
 
 // Created by Andrew
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 public class FirebaseUtils {
 
-    public static FirebaseDatabase DB = FirebaseDatabase.getInstance();
-    public static DatabaseReference usersRef = DB.getReference("users");
-    public static DatabaseReference gamesRef = DB.getReference("games");
+    public static final FirebaseDatabase DB = FirebaseDatabase.getInstance();
+    public static final DatabaseReference usersRef = DB.getReference("users");
+    public static final DatabaseReference gamesRef = DB.getReference("games");
 
     public FirebaseUtils() {
         // Nothing here.
     }
 
     public static int getNewGameID() {
-        return 314;
+        Random random = new Random();
+        return random.nextInt(9000) + 1000;
     }
 
     public static boolean doesUsernameExist(String username) {
         return false;
     }
 
-    public static boolean doesGameExist(int id) {
-        return false;
+    public static boolean doesGameExist(int gameID) {
+        return true;
     }
 
-    public static void createUser(String username) {
-        DatabaseReference userRef = usersRef.child(username);
-        userRef.child("score").setValue(0);
+    public static int getNewPlayerUID() {
+        Random random = new Random();
+        return random.nextInt(900000) + 100000;
+    }
+
+    public static void createUser(int uid, String name, String email) {
+        DatabaseReference userRef = usersRef.child(Integer.toString(uid));
+        userRef.child("email").setValue(email);
+        userRef.child("game").setValue(0); // 0 means not currently in a game
+        userRef.child(name).setValue(name);
     }
 
     public static List<Word> getNewWordList() {
-        Word tree = new Word("tree", "arbol");
-        ArrayList<Word> wordList = new ArrayList<>();
-        wordList.add(tree);
-        return wordList;
+        return Word.getDummyData();
     }
 
     public static void createGame(Game game) {
-        DatabaseReference gameRef = gamesRef.child(Integer.toString(game.getJoinID()));
-        gameRef.child("player_list").setValue(game.getPlayerList());
+        // Firebase only likes String dbref names
+        String gameKey = Integer.toString(game.getJoinID());
+        String creatorKey = Integer.toString(game.getCreatorUID());
+
+        // Put the game in the "games" section
+        DatabaseReference gameRef = gamesRef.child(gameKey);
+        gameRef.child("words").setValue(game.getWordList());
+
+        DatabaseReference playerListRef = gameRef.child("player_list");
+
+        DatabaseReference playerRef;
+        for (int playerUID : game.getPlayerList()) {
+            playerRef = playerListRef.child(Integer.toString(playerUID));
+            playerRef.child("points").setValue(game.getPlayerScore(playerUID));
+        }
+
+        // Set player's game attribute to new game id
+        DatabaseReference userRef = usersRef.child(creatorKey);
+        userRef.child("game").setValue(game.getJoinID());
     }
 
-    public static void playerJoinGame(String player, int gameID) {
-        if (!doesGameExist(gameID)) {
-            System.out.println("Game does not exist.");
-            return;
-        }
+    public static void playerJoinGame(int playerUID, int gameID) {
+        System.out.println("Attempting to add player " + playerUID + " to game " + gameID + ".");
+
+        DatabaseReference gameRef = getGameRefFromID(gameID);
+        DatabaseReference newPlayerRefInGame = gameRef.child("player_list").child(Integer.toString(playerUID));
+        newPlayerRefInGame.child("points").setValue(0);
+
+        usersRef.child(Integer.toString(playerUID)).child("game").setValue(gameID);
+    }
+
+    public static void setWordsFound(int playerUID, int gameID, List<Word> words) {
+        DatabaseReference gameRef = getGameRefFromID(gameID);
+        DatabaseReference playerRef = gameRef.child("player_list").child(Integer.toString(playerUID));
+        DatabaseReference wordsFoundRef = playerRef.child("words_found");
+        wordsFoundRef.setValue(words);
+    }
+
+    public static DatabaseReference getGameRefFromID(int gameID) {
+        return gamesRef.child(Integer.toString(gameID));
+    }
+
+    public static String getUsernameFromUID(int uid) {
+        DatabaseReference userRef = usersRef.child(Integer.toString(uid));
+        return "andrew";
     }
 
 }
--- app/src/main/java/teamc/finalproject/Game.java
+++ app/src/main/java/teamc/finalproject/Game.java
@@ -2,39 +2,64 @@ package teamc.finalproject;
 
 // Created by Andrew
 
+import android.support.v4.util.ArrayMap;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Game {
 
-    private String creator;
+    private int creatorUID;
     private int joinID;
-    private List<String> playerList;
-    private Map<String, Integer> scores;
+    private Map<Integer, Integer> scores; // Acts as the player list
+    private List<Word> wordList;
 
-    public Game(String creator, int joinID) {
-        this.creator = creator;
+    public Game(int creatorUID, int joinID, List<Word> wordList) {
+        this.creatorUID = creatorUID;
         this.joinID = joinID;
+        this.wordList = wordList;
 
-        playerList = new ArrayList<>();
-        playerList.add(creator);
-
-        scores = new HashMap<>();
-        setPlayerScore(creator, 0);
+        scores = new ArrayMap<>();
+        // setPlayerScore(creatorUID, 0);
     }
+
+    public Game(int creatorUID) {
+        this(creatorUID, FirebaseUtils.getNewGameID(), FirebaseUtils.getNewWordList());
+    }
+
+    public void givePlayerPoints(int playerUID, int points) {
+        int newPlayerScore = scores.get(playerUID) + points;
+        setPlayerScore(playerUID, newPlayerScore);
+    }
+
+    public void setPlayerScore(int playerUID, int score) {
+        scores.put(playerUID, score);
+    }
+
+    public void playerJoinGame(int playerUID) {
+        setPlayerScore(playerUID, 0);
+    }
+
+    // Getters
 
     public int getJoinID() {
         return joinID;
     }
 
-    public List<String> getPlayerList() {
-        return playerList;
+    public List<Integer> getPlayerList() {
+        return new ArrayList<>(scores.keySet());
     }
 
-    public void setPlayerScore(String username, int score) {
-        scores.put(username, score);
+    public int getCreatorUID() {
+        return creatorUID;
+    }
+
+    public List<Word> getWordList() {
+        return this.wordList;
+    }
+
+    public int getPlayerScore(int playerUID) {
+        return scores.get(playerUID);
     }
 
 }
